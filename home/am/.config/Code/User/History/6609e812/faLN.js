9f729a54021566aa42098a4268e7cc8c46682b07
Adrian Moreno <adrianemoreno.25@gmail.com>
2022-06-26 23:32:49 -0400

    adding vscode config

---
+++ home/am/.config/Code/User/History/6609e812/faLN.js
@@ -0,0 +1,61 @@
+const Usuario = require('../models/Usuario.js');
+const { check, validationResult } = require('express-validator');
+
+module.exports = {
+
+	index: async (req, res) => {
+		const u = await Usuario.findAll({
+			order: [
+				['id', 'asc']
+			]
+		});
+		res.render('usuario/index', {usuarios: u});
+	},
+
+	create: async (req, res) => {
+		try {
+			await Usuario.create({
+				nombre: req.body.nombre,
+				pass: req.body.pass,
+			});
+
+			res.send('Usuario creado correctamente');
+
+		} catch(e) {
+			res.send('Error ' + e);
+		}
+	},
+
+	update: async (req, res) => {
+		try {
+			await Usuario.update({
+				nombre: req.body.nombre,
+				pass: req.body.pass,
+			},{
+				where: {
+					id: req.params.id
+				}
+			});
+
+			res.send('Usuario actualizado correctamente');
+
+		} catch(e) {
+			res.send('Error ' + e)
+		}
+	},
+
+	destroy: async (req, res) => {
+		try {
+			await Usuario.destroy({
+				where: {
+					id: req.params.id
+				}
+			});
+
+			res.send('Usuario borrado correctamente');
+
+		} catch(e) {
+			res.send('Error ' + e);
+		}
+	},
+}
